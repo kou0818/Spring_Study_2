6570e2d4d85d6a3e6658db73f00aa1add1eb0569
kodai fukuda <k-fukuda057@jutjoy.com>
2026-03-11 21:37:35 +0900

    tesu

---
+++ javasilver/src/main/java/javasilver/javacheck.java
@@ -0,0 +1,13 @@
+package javasilver;
+
+public class javacheck {
+
+	public static void main(String[] args) {
+
+String[][] array = {{ "A", "B", "C"}};
+for (Object obj : array) {
+	System.out.println(obj);
+}
+	}
+
+}
